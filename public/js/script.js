99c3339a36acea84e8a80f1e9ebfdf50ab85c3ee
Signo <signo@jupr.fr>
2018-10-13 11:51:04 +0200

    init

---
+++ public/js/script.js
@@ -0,0 +1,380 @@
+var CENTER = [2.3859, 48.8500];
+var ZOOM = 12.5;
+var RESOLUTION = 7;
+var MOUSEH3 = h3.geoToH3(CENTER[1], CENTER[0], RESOLUTION);
+var MOUSECOORDS = null;
+//var IDXGRID = {};
+var TILES = {};
+
+var defaultsHash = {
+	"871fb4662ffffff": {
+		"type": "Feature",
+		"geometry": h3.h3ToGeo("871fb4662ffffff", true)
+	},
+	"871fb4671ffffff": {
+		"type": "Feature",
+		"geometry": h3.h3ToGeo("871fb4671ffffff", true)
+	},
+	"871fb4675ffffff": {
+		"type": "Feature",
+		"geometry": h3.h3ToGeo("871fb4675ffffff", true)
+	}
+};
+
+var h3Dataset = {};
+
+/*
+//
+*/
+
+var image = new ol.style.Circle({
+	radius: 5,
+	fill: null,
+	stroke: new ol.style.Stroke({color: 'red', width: 1})
+});	
+
+var styles = {
+	'Point': new ol.style.Style({
+		image: image
+	}),
+	'LineString': new ol.style.Style({
+		stroke: new ol.style.Stroke({
+			color: 'green',
+			width: 1
+		})
+	}),
+	'MultiLineString': new ol.style.Style({
+		stroke: new ol.style.Stroke({
+			color: 'green',
+			width: 1
+		})
+	}),
+	'MultiPoint': new ol.style.Style({
+		image: image
+	}),
+	'MultiPolygon': new ol.style.Style({
+		stroke: new ol.style.Stroke({
+			color: 'yellow',
+			width: 1
+		}),
+		fill: new ol.style.Fill({
+			color: 'rgba(255, 255, 0, 0.1)'
+		})
+	}),
+	'Polygon': new ol.style.Style({
+		stroke: new ol.style.Stroke({
+			color: "rgba(255, 0, 0, .33)",
+			lineDash: [15],
+			width: 10
+		}),
+		fill: new ol.style.Fill({
+			color: 'rgba(255, 255, 0, 0.0)'
+		})
+	}),
+	'GeometryCollection': new ol.style.Style({
+		stroke: new ol.style.Stroke({
+			color: 'magenta',
+			width: 2
+		}),
+		fill: new ol.style.Fill({
+			color: 'magenta'
+		}),
+		image: new ol.style.Circle({
+			radius: 10,
+			fill: null,
+			stroke: new ol.style.Stroke({
+				color: 'magenta'
+			})
+		})
+	}),
+	'Circle': new ol.style.Style({
+		stroke: new ol.style.Stroke({
+			color: 'red',
+			width: 2
+		}),
+		fill: new ol.style.Fill({
+			color: 'rgba(255,0,0,0.2)'
+		})
+	})
+};
+
+var styleFunction = function(feature) {
+	return styles[feature.getGeometry().getType()];
+};
+
+/*
+//
+*/
+
+var layerOSM = new ol.layer.Tile({
+	name: "swailShom",
+	type: "base",
+	visible: true,
+	source: new ol.source.XYZ({
+		url: "http://{a-c}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png",
+		//crossOrigin: 'anonymous'
+	}),
+	renderMode: "image",
+});
+
+var layerMousePosition = new ol.layer.Vector({
+	name: "mousePosition",
+	type: "base",
+	visible: true,
+	source: new ol.source.Vector({
+		features: [
+			new ol.Feature({
+				geometry: new ol.geom.Point(ol.proj.fromLonLat(CENTER)),
+				id: "targetMouse"
+			})
+		]
+	}),
+	renderMode: "vector",
+	style: styleFunction,
+});
+
+var layerCells = new ol.layer.Vector({
+	name: "cells",
+	type: "base",
+	visible: true,
+	source: new ol.source.Vector({
+		features: []
+	}),
+	style: styleFunction,
+	renderMode: "image",
+	renderBuffer: 1024
+});
+
+var map = new ol.Map({
+	controls: ol.control.defaults({
+		attributionOptions: {
+			collapsible: false
+		}
+	}).extend([]),
+	layers: [
+		layerOSM,
+		layerMousePosition,
+		layerCells,
+	],
+	target: "map",
+	view: new ol.View({
+		center: ol.proj.fromLonLat(CENTER),
+		zoom: ZOOM,
+	})
+});
+
+/*
+//
+*/
+
+var polygonH3To4326 = function(coords) {
+	for (var i in coords) {
+		coords[i] = ol.proj.fromLonLat(coords[i]);
+	}
+	return coords;
+};
+
+var updateHexCursor = function(lat, lon, resolution) {
+	var hash = h3.geoToH3(lat||centerInit[1], lon||centerInit[0], resolution||RESOLUTION);
+	if (MOUSEH3 !== hash) {
+		MOUSEH3 = hash;
+		var hexagon = h3.h3ToGeoBoundary(hash, true);
+		map.getLayers().forEach(function(layer) {
+			if (layer.get("name") === "mousePosition") {
+				var polygon = new ol.geom.Polygon([polygonH3To4326(hexagon)]);
+				layer.getSource().getFeatures()[0].set("hash", hash);
+				layer.getSource().getFeatures()[0].setGeometry(polygon);
+				console.log("[CURSOR]", "R"+RESOLUTION, hash);
+			}
+		});
+	}
+};
+
+map.on("pointermove", function(evt) {
+	if (evt.dragging) return;
+	MOUSECOORDS = evt.coordinate;
+	var point3857To4326 = ol.proj.transform(evt.coordinate, "EPSG:3857", "EPSG:4326");
+	updateHexCursor(point3857To4326[1], point3857To4326[0]);
+});
+
+var resolutionForm = document.getElementById("resolution");
+resolutionForm.addEventListener("change", function(event) {
+	console.log("FORM", event);
+	RESOLUTION = event.target.valueAsNumber;
+});
+
+map.on("click", _.debounce(function(evt) {
+	if (evt.originalEvent.shiftKey === true) {
+		map.forEachFeatureAtPixel(evt.pixel, function(feature, layer) {
+			insertCell(feature);
+		});
+	}
+}, 10));
+
+document.addEventListener("keydown", function(event) {
+	if (event.which === 81) { // KEY Q : RESOLUTION DOWN
+		if (RESOLUTION<15) RESOLUTION+=1;
+		document.getElementById("resolution").value = RESOLUTION;
+		console.log("[RESOLUTION]", "R"+(RESOLUTION-1), "to", "R"+RESOLUTION);
+	}
+	if (event.which === 87) { // KEY W : RESOLUTION UP
+		if (RESOLUTION>0) RESOLUTION+=-1;
+		document.getElementById("resolution").value = RESOLUTION;
+		console.log("[RESOLUTION]", "R"+(RESOLUTION+1), "to", "R"+RESOLUTION);
+	}
+});
+
+var insertCell = function(feature) {
+	var hash = feature.get("hash");
+	if (hash && h3.h3IsValid(hash) && !h3Dataset[hash]) {
+		var projOl = feature.getGeometry().getCoordinates();
+		var proj84 = [[]];
+		for (var i in projOl[0]) {
+			proj84[0][i] = ol.proj.transform(projOl[0][i], 'EPSG:3857', 'EPSG:4326')
+		}
+		//console.log("feature:", proj84);
+		var cell = feature.clone();
+		var cellStyle = new ol.style.Style({
+			stroke: new ol.style.Stroke({
+				color: "rgba(255, 0, 0, .33)",
+				lineDash: [8],
+				width: 5
+			}),
+			fill: new ol.style.Fill({
+				color: "rgba(255, 255, 255, .0)"
+			}),
+			text: new ol.style.Text({
+				font: "16px Quicksand, sans-serif",
+				text: feature.get("hash"),
+				fill: new ol.style.Fill({
+					color: "red"
+				}),
+					stroke: new ol.style.Stroke({
+					color: "white",
+					width: 3
+				})
+			})
+		});
+		cell.setStyle(cellStyle);
+		layerCells.getSource().addFeature(cell);
+		h3Dataset[cell.get("hash")] = cell;
+		h3String += hash+"\n";
+		$("textarea#h3").text(h3String);
+		var geosjon = JSON.parse(h3Geojson);
+		geosjon.features.push({
+			"type": "Feature",
+			"geometry": {
+				"type": "Polygon",
+				"coordinates": proj84,
+			},
+			"properties": {
+				"h3": hash,
+			},
+		});
+		h3Geojson = JSON.stringify(geosjon);
+		$("textarea#geojson").text(h3Geojson);
+		console.log("[CELL]:", cell.get("hash"), "cell added");
+	}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var panel = "h3";
+
+var switchPanel = function(id) {
+	if (id === "geojson") {
+		panel = id;
+		$("#geojson").show();
+		$("#h3").hide();
+	}
+	if (id === "h3") {
+		panel = id;
+		$("#h3").show();
+		$("#geojson").hide();
+	}
+};
+
+var h3String = "";
+var h3Geojson = "{\"type\":\"FeatureCollection\",\"features\":[]}";
+
+for (var hash in defaultsHash) {
+	var hexagon = h3.h3ToGeoBoundary(hash, true);
+	var polygon = new ol.geom.Polygon([polygonH3To4326(hexagon)]);
+	var feature = new ol.Feature({
+		geometry: polygon,
+		id: hash
+	});
+	feature.values_.hash = hash;
+	insertCell(feature);
+}
+
+//871fb4660ffffff
+$("textarea#h3").bind("input propertychange", function(evt) {
+	if (panel === "h3") {
+		var newVal = $("textarea#h3").val();
+		if (newVal !== h3String) {
+			var splitted = h3String.split("\n");
+			console.log("splitted", splitted)
+			for (var i in splitted) {
+				if (newVal.match(splitted[i])[0] !== "") {
+					console.log("MATCH");
+				} else {
+					console.log("UNMATCH");
+				}
+			}
+		}
+	}
+});
+
+$("textarea#geojson").bind("input propertychange", function(evt) {
+	if (panel === "geojson") console.log("evt:", evt, $("textarea#geojson").val());
+});
+
+switchPanel("h3");
+
+var openPanel = function() {
+  $("#panel-open-button").hide();
+	$("#panel").animate({
+    right: "0px",
+  }, 666, function() {
+  	//
+  });
+};
+
+var closePanel = function() {
+	$("#panel").animate({
+    right: "-425px",
+  }, 666, function() {
+    $("#panel-open-button").show();
+  });
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ public/lib/h3-js/CHANGELOG.md
@@ -0,0 +1,19 @@
+# Change Log
+
+All notable changes to this project will be documented in this file. This library adheres to a versioning policy described in [the README](./README.md#versioning). The public API of this library consists of the functions exported in [h3core.js](./lib/h3core.js).
+
+## [Unreleased]
+
+## [3.0.2] - 2018-07-26
+### Changed
+- Updated the core library to v3.0.8 (#10)
+- Renamed names of h3.1 or h3-1 to h3 (#4)
+- Added engine support for Node 10 (#11)
+
+## [3.0.1] - 2018-06-18
+### Fixed
+- Fixed npm distribution
+
+## [3.0.0] - 2018-06-18
+### Added
+-   First public release.
